ae595111d44dfee189d3a29268b6469d20fddc82
Cisenz <cisnez@icloud.com>
2023-05-07 23:41:00 -0600

    Remove template files from main branch and ignore them

--- B17_73L36R4M.py
+++
@@ -1,5 +0,0 @@
-#B17_73L36R4M.py
-class T3L36R4M:
-    def __init__(self, api_key, bot_token, bot_init_data):
-        # Initialize the Telegram bot here
-        pass
--- B17_D15C0RD.py
+++
@@ -1,5 +0,0 @@
-#B17_D15C0RD.py
-class D15C0RD:
-    def __init__(self, api_key, bot_token, bot_init_data):
-        # Initialize the Discord bot here
-        pass
